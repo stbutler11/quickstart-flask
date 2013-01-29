283ae9aa0eb0a0ec0ec22e8d972cc52aee3f9b69
stbutler11 <stbutler11@hotmail.com>
2013-01-29 21:55:10 +0000

    Added template

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/hello/')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
